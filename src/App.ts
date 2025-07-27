80b03dc18a8d16fd43d623bb53d288775042f0e3
zhongtao <ztappleemail@163.com>
2025-07-27 17:27:18 +0800

    refactor(src): 优化 App.ts 中的状态管理
    - 将 user 和 password 变量改为使用 ref 函数创建的响应式对象- 初始化 dept 为一个空字符串，而不是 'top'
    - 修正

--- src/App.ts
+++ src/App.ts
@@ -1,12 +1,12 @@
 import { ref } from 'vue'
 
 export default function () {
-    let user = ''
-    let password = ''
-    let dept = ref( 'top')
+    let user = ref( '')
+    let password = ref( '')
+    let dept = ref( '')
     function userLogin() {
         alert('登录成功')
-        dept.value = 'x'
+        dept.value = 'top'
         console.log('用户名：', user)
         console.log('密码：', password)
         console.log('部门：', dept.value)
