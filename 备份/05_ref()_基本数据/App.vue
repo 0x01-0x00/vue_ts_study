bd4213ce103f4e0e679ea3601be0dc3f9678c0bd
zhongtao <ztappleemail@163.com>
2025-08-06 10:15:14 +0800

    feat(备份): 添加 ref 和 reactive 示例代码
    - 新增 05_ref()_基本数据 和07_reactive()_对象类型 目录
    - 添加 App.vue 和 Person.vue 组件
    - 实现了使用 ref 和 reactive 创建响应式数据的示例
    - 添加了修改数据和显示数据的方法

---
+++ 备份/05_ref()_基本数据/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Person></Person>
+</template>
+
+<script lang="ts">
+  import Person from './components/Person.vue'
+
+  export default {
+    name:'App', // 组件名
+    components: { // 注册组件
+      Person
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ 备份/05_ref()_基本数据/components/Person.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="person">
+    <h2>姓名：{{name}}</h2>
+    <h2>年龄：{{age}}</h2>
+    <button @click="changeName">修改名字</button>
+    <button @click="changeAge">修改年龄</button>
+    <button @click="showTel">查看联系方式</button>
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'Person',
+  }
+</script>
+
+<script setup lang="ts">
+    import { ref } from 'vue'
+
+    // data
+    let name = ref('张三') // 响应式数据
+    let age = ref(18)
+    let tel = '18269776130'
+
+    console.log(name)
+
+    // methods
+    function changeName() {
+      name.value = '钟涛'
+      console.log(name.value)
+    }
+    function changeAge() {
+      age.value += 1
+      console.log(age.value)
+    }
+    function showTel() {
+      alert(tel)
+    }
+</script>
+
+<style scoped>
+  .person {
+    background-color: skyblue;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+  }
+</style>
